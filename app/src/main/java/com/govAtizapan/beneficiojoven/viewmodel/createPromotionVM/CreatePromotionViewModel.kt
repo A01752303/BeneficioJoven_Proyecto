a6a409906dd4ebe6611310fab128590d13b2a541
tadeo2006 <tademanuelcode@outlook.com>
2025-10-16 21:51:16 -0600

    Cambios de la UI de CreatePromocionesScreen y actualizacion para formatos diferentes en la base de datos.

--- app/src/main/java/com/govAtizapan/beneficiojoven/viewmodel/createPromotionVM/CreatePromotionViewModel.kt
+++ app/src/main/java/com/govAtizapan/beneficiojoven/viewmodel/createPromotionVM/CreatePromotionViewModel.kt
@@ -3,7 +3,6 @@ package com.govAtizapan.beneficiojoven.viewmodel.createPromotionVM
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.govAtizapan.beneficiojoven.model.network.RetrofitClient
-import com.govAtizapan.beneficiojoven.model.promotionpost.PromotionRequest
 import com.govAtizapan.beneficiojoven.model.promotionpost.PromotionType
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
