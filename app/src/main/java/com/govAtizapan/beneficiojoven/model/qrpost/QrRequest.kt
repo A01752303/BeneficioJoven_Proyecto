6b01789496a189c4802c4fa49b3fe8d87918e33e
jac2k577 <A01800766@tec.mx>
2025-10-21 18:32:06 -0600

    Correccion qr generacion

--- app/src/main/java/com/govAtizapan/beneficiojoven/model/qrpost/QrRequest.kt
+++ app/src/main/java/com/govAtizapan/beneficiojoven/model/qrpost/QrRequest.kt
@@ -3,6 +3,6 @@ package com.govAtizapan.beneficiojoven.model.qrpost
 import com.google.gson.annotations.SerializedName
 
 data class QrRequest(
-    @SerializedName("id_usuario") val idUsuario: Int,
+
     @SerializedName("id_promocion") val idPromocion: Int
 )
